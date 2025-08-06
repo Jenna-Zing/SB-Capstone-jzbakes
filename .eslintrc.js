7dbcd519ecf451f002c4ea11cc6cedca264bce56
Jenna-Zing <47491259+Jenna-Zing@users.noreply.github.com>
2025-08-05 23:57:43 -0500

    added some eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,10 +11,13 @@ module.exports = {
   extends: [
     'eslint:recommended', // base ESLint recommended rules
     'plugin:@typescript-eslint/recommended', // recommended rules from the TypeScript ESLint plugin
-    'plugin:prettier/recommended', // enables prettier plugin and disables conflicting ESLint rules
+    'plugin:prettier/recommended', // enables prettier plugin and disables conflicting ESLint rules - meaning, it enforces both prettier and ESLint rules when running eslint
   ],
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
     'prettier/prettier': 'error', // show prettier issues as ESLint errors
+    'no-unused-vars': 'error', // flag unused variables
+    semi: ['error', 'always'], // require semicolons
+    eqeqeq: ['error', 'always'], // enforce strict equality checks (e.g. Must use === instead of ==)
   },
 };
