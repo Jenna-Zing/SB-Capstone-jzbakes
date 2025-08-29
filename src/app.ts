3d76dd0e15b89eaaa3a5f0a912b65641a2ff04c6
Jenna-Zing <47491259+Jenna-Zing@users.noreply.github.com>
2025-08-29 18:07:58 -0500

    fixed API routing so it links to FE

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'; // dotenv: Loads environment variables from a .env 
 dotenv.config(); // loads the environment variables into process.env.
 
 import express from 'express'; // loads the express module into the project so you can build your server
+import cors from 'cors'; // cors: Middleware that allows your server to accept requests from different origins (e.g. React FE app which runs on port 3000, while this BE server runs on port 8080)
 
 import routes from './routes'; // imports your routes from the src/routes/index.js file so Express knows what to do when users visit URLs
 import usersRoutes from './routes/users';
@@ -13,16 +14,21 @@ import orderItemsRoutes from './routes/orderItems';
 
 const app = express(); // creates an instance of an Express app
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, // allows requests from the FE React app running on port 5173
+  })
+); // enables CORS so your server can accept requests from different origins (e.g. React FE app which runs on port 5173, while this BE server runs on port 8080)
 app.use(express.json()); // Built-in Express middleware that lets you read JSON data from incoming requests (e.g. POST requests with JSON bodies).
 
 // DEFINING ROUTES
-app.use('/', routes); // this tells Express when a user visits any route starting with '/', use the logic from the routes file
+app.use('/api', routes); // this tells Express when a user visits any route starting with '/', use the logic from the routes file
 app.use('/api/users', usersRoutes);
 app.use('/api/orders', ordersRoutes);
 app.use('/api/products', productsRoutes);
 app.use('/api/orderItems', orderItemsRoutes);
 
-const PORT = process.env.PORT || 8080; // sets a port number - use special port number from env file var; if not, defaults to 3000
+const PORT = process.env.PORT || 8080; // sets a port number - use special port number from env file var; if not, defaults to 5173
 
 // Starts the server and listens for requests. Once it’s running, it prints a message to the terminal.
 app.listen(PORT, () => {
--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,8 +1,27 @@
 import { Request, Response } from 'express';
+import pool from '../db';
 
-export const getProducts = (req: Request, res: Response) => {
-  res.json([
-    { id: 1, name: 'Phone', price: 699 },
-    { id: 2, name: 'Laptop', price: 1200 },
-  ]);
+export const getProducts = async (req: Request, res: Response) => {
+  // res.json([
+  //   { id: 1, name: 'Phone', price: 699 },
+  //   { id: 2, name: 'Laptop', price: 1200 },
+  // ]);
+  try {
+    const query =
+      'SELECT id, name, CAST(cost AS INTEGER), description, ingredients, "imgURL" FROM product ORDER BY name ASC';
+    const result = await pool.query(query);
+
+    console.log('Products fetched from DB:', result.rows);
+
+    res.status(200).json({ products: result.rows });
+    // res.json({
+    //   success: true,
+    //   products: result.rows,
+    // });
+  } catch (err) {
+    console.error('Database connection failed:', err);
+    res
+      .status(500)
+      .json({ success: false, error: 'Database connection failed' });
+  }
 };
