824388bb0732d7ced730a726e91c7ab4c657d994
Jenna-Zing <47491259+Jenna-Zing@users.noreply.github.com>
2025-09-29 19:09:28 -0500

    removed commented out code in getProducts API

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -2,10 +2,6 @@ import { Request, Response } from 'express';
 import pool from '../db';
 
 export const getProducts = async (req: Request, res: Response) => {
-  // res.json([
-  //   { id: 1, name: 'Phone', price: 699 },
-  //   { id: 2, name: 'Laptop', price: 1200 },
-  // ]);
   try {
     const query =
       'SELECT id, name, CAST(cost AS INTEGER), description, ingredients, "imgURL" FROM product ORDER BY name ASC';
@@ -14,10 +10,6 @@ export const getProducts = async (req: Request, res: Response) => {
     console.log('Products fetched from DB:', result.rows);
 
     res.status(200).json({ products: result.rows });
-    // res.json({
-    //   success: true,
-    //   products: result.rows,
-    // });
   } catch (err) {
     console.error('Database connection failed:', err);
     res
